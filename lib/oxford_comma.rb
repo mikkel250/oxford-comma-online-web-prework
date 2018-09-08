93a9e0b62bf9f80621a45955b5889f9ed8ec5c13
Mikkel Ridley <mikkel250@gmail.com>
2018-09-08 04:05:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    str = array.join(", ")
+    str[-2] = " and "
+    str
+  end
+  
 end
+# note just add the " and " to the string
