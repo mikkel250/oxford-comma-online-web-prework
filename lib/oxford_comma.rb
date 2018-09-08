1a9a60f2b12094a60005159406e263027906d95d
Mikkel Ridley <mikkel250@gmail.com>
2018-09-08 04:11:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length > 2
-    temp_arr
+    temp_arr = []
     for i in array.length[0..-2]
       temp_arr << (i + ", ")
     end
