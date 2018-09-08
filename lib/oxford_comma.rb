ae7dd65b75bb5466f44b9cbe8948ec36a63892d0
Mikkel Ridley <mikkel250@gmail.com>
2018-09-08 04:15:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    temp_arr = []
+    for i in array[0..-2]
+      temp_arr << (i + ", ")
+    end
+    last = "and #{array[-1]}"
+    temp_arr << (last)
+    temp_arr.join
+  end
+  
 end
+# note just add the " and " to the string
